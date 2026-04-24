4e67241cae8264f773a19e478a59f6accbd1f19c
BaldrProgrammer <boliklevik@gmail.com>
2026-04-24 11:43:33 +0200

    dz zad1 1

---
+++ sharp_zadania/dziedziczenie/zad1.cs
@@ -0,0 +1,34 @@
+﻿namespace sharp_zadania.dziedziczenie;
+
+public class Figura
+{
+    public void Pole()
+    {
+        Console.WriteLine("to nie jest figura konkretna");
+    }
+}
+
+
+public class Kolo : Figura
+{
+    public void Pole(int radius)
+    {
+        Console.WriteLine($"Pole: {3.1415 * (radius * radius)}");
+    }
+}
+
+public class Trojkat : Figura
+{
+    public void Pole(int strona, int wysokosc)
+    {
+        Console.WriteLine($"Pole: {0.5 * strona * wysokosc}");
+    }
+}
+
+public class Prostokat : Figura
+{
+    public void Pole(int strona1, int strona2)
+    {
+        Console.WriteLine($"Pole: {strona1 * strona2}");
+    }
+}
