290c64fbbabc9eb48f80e136368b03b96fb74a30
BaldrProgrammer <boliklevik@gmail.com>
2026-04-24 12:11:55 +0200

    dz zad2 1

---
+++ sharp_zadania/dziedziczenie/zad2.cs
@@ -0,0 +1,37 @@
+﻿namespace sharp_zadania.dziedziczenie;
+
+public class Osoba
+{
+    private string _imie;
+    private string _nazwisko;
+    private string _pesel;
+
+    public Osoba(string imie, string nazwisko, string pesel)
+    {
+        _imie = imie;
+        _nazwisko = nazwisko;
+        _pesel = pesel;
+    }
+}
+
+public class Student : Osoba
+{
+    private string _nrIndeksu;
+    
+    public Student(string imie, string nazwisko, string pesel, string nrIndeksu)
+        : base(imie, nazwisko, pesel)
+    {
+        _nrIndeksu = nrIndeksu;
+    }
+}
+
+public class Pracownik : Osoba
+{
+    private string _praca;
+    
+    public Pracownik(string imie, string nazwisko, string pesel, string praca)
+        : base(imie, nazwisko, pesel)
+    {
+        _praca = praca;
+    }
+}
