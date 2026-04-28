7a6c6ec19c8a36aa65ebcb3a6f627a3182f30596
BaldrProgrammer <boliklevik@gmail.com>
2026-04-28 12:19:46 +0200

    zad3 1

---
+++ sharp_zadania/polimorfism/zad3.cs
@@ -0,0 +1,30 @@
+﻿namespace sharp_zadania.polimorfism;
+
+abstract class Pracownik
+{
+    public abstract double ObliczPlace();
+}
+
+class Stazysta : Pracownik
+{
+    public override double ObliczPlace()
+    {
+        return  1.5;
+    }
+}
+
+class Etatowy : Pracownik
+{
+    public override double ObliczPlace()
+    {
+        return  1.5;
+    }
+}
+
+class Kontraktowy : Pracownik
+{
+    public override double ObliczPlace()
+    {
+        return  1.5;
+    }
+}
