a1fe1a57b6b06053c4e001bbe7a76e5fb6d779c9
balddrAdmin <boliklevik@gmail.com>
2026-05-03 00:51:10 +0200

    zad5 completed

--- sharp_zadania/polimorfism/zad5.cs
+++ sharp_zadania/polimorfism/zad5.cs
@@ -1,5 +1,10 @@
 namespace sharp_zadania.polimorfism;
 
+/// Porównaj z podejściem dziedziczenia
+/// no tu opisujesz ze jakas klasa musi pelnic cos,
+/// a w dziedziczeniu tworzysz klase na podstawie juz czegos stworzonego
+
+
 public interface IPlatable
 {
     void Oplac();
