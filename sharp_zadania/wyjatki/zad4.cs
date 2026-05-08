ead97f03e1294d6a01f9afe3eb733ea1ca900bfb
BaldrProgrammer <boliklevik@gmail.com>
2026-05-08 11:01:56 +0200

    zad4 `

---
+++ sharp_zadania/wyjatki/zad4.cs
@@ -0,0 +1,18 @@
+﻿namespace sharp_zadania.wyjatki;
+
+public class zad4
+{
+    public void Main()
+    {
+        try
+        {
+            int a = 5;
+            Console.WriteLine(a / 0);
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
