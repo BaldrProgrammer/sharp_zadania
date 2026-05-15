e5cdec63aa1bf60da20100cde9fd027265e3b91e
BaldrProgrammer <boliklevik@gmail.com>
2026-05-15 10:51:16 +0200

    zad5 1

---
+++ sharp_zadania/wyjatki/zad5.cs
@@ -0,0 +1,26 @@
+﻿namespace sharp_zadania.wyjatki;
+
+public class Zasob : IDisposable
+{
+    private string _dbUrl;
+    
+    public Zasob(string dbUrl)
+    {
+        _dbUrl = dbUrl;
+        Console.WriteLine($"Zasob bazy {_dbUrl}");
+    }
+
+    public void Wykorzystac()
+    {
+        Console.WriteLine($"wykorzystane jest {_dbUrl}");
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+    
+    ~Zasob(){
+        Console.WriteLine($"Zasob zniszczony");
+    }
+}
